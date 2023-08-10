35ffa706982b4611e9e2f34f66d9d4985bdd0bd3
Toma Cristian <tomacristian10@yahoo.com>
2023-08-10 20:19:06 +0300

    10 august

--- curs05/email.py
+++ curs05/email.py
@@ -2,15 +2,14 @@
 litere_sau_cifre@litere_sau_cifre.litere
 Validati acest sir de caractere si informati utilizatorul de raspuns. @ sau .(punct) trebuie sa existe o singura data
 in sirul de caractere."""
+
 email_adress = input("Adauga adresa de email: ")
-# if email_adress.split('@')[0] .isalnum() is False:
-#     email_adress = input(f"Adresa de email {email_adress} nu este valida. Adauga din nou adresa de email: ")
 while email_adress.count('@') != 1 or \
         email_adress.count('.') != 1 or \
         email_adress.index('@') > email_adress.index('.') or \
         email_adress.split('@')[0] .isalnum() is False or\
         email_adress.split('@')[1].split('.')[0].isalnum() is False or \
-        email_adress.split('@')[1].split('.')[1].isalpha() is False:
+        email_adress.split('@')[1].split('.')[1].isalpha() is False or \
+        len(email_adress.split('.')[1]) < 2:
     email_adress = input(f"Adresa de email {email_adress} nu este valida. Adauga din nou adresa de email: ")
 print(f"Adresa de email {email_adress} este valida.")
-
