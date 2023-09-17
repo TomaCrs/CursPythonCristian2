a5f64f8cbafdbc19c498960265da8da6a90141cb
Toma Cristian <tomacristian10@yahoo.com>
2023-09-17 12:41:40 +0300

    exam

---
+++ Teme/mid_exam2.py
@@ -0,0 +1,64 @@
+# ex1
+nr_chei = input("Introduceti nr chei(maxim 20): ")
+while nr_chei.isdigit() is False or int(nr_chei) not in range(21):
+    nr_chei = input("Introduceti nr chei(maxim 20): ")
+dictionar = {}
+for index in range(1,int(nr_chei)+1):
+    dictionar.update({f'{index}': index * index})
+print(dictionar)
+
+#ex2
+def suma_tuplu(tuplu: tuple) -> int:
+    suma = 0
+    for x in tuplu:
+        suma += x
+    return suma
+
+
+tuple_example = (8, 2, 3, 0, 7)
+print(suma_tuplu(tuple_example))
+
+#ex3
+def separere_lista(lista: list, nr: int) -> list:
+    result =[]
+    result.append(lista[0:nr])
+    result.append(lista[nr:])
+    return result
+
+
+list_example = [1, 1, 2, 3, 4, 4, 5, 1]
+nr = input("Introduceti numarul: ")
+while nr.isdigit() is False or int(nr) > len(list_example):
+    nr = input("Introduceti numarul: ")
+print(separere_lista(list_example, int(nr)))
+
+#ex4
+sir = "cristian"
+counter = 0
+for x in sir:
+    if x in "aeiou":
+        counter += 1
+print(f"{counter} vocale")
+
+#ex5
+print("Introduceti 3 numere: ")
+x = int(input())
+y = int(input())
+z = int(input())
+if x==0 or y==0 or z==0:
+    print(0)
+elif x==y==z:
+    print(3*x/y/z)
+else:
+    print(x/y/z)
+
+#ex6
+n = [1, 2, 3, 4, 5, 6, 7]
+i = 0
+while i < len(n):
+    x = n[i]
+    if x % 2 == 0:
+        n.insert(i+1, x*2)
+        i += 1
+    i += 1
+print(n)
