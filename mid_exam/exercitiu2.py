a6299f7c85947ee31ff628ad6bf87e3063f2a135
Toma Cristian <tomacristian10@yahoo.com>
2023-09-21 20:06:08 +0300

    mid_exam

---
+++ mid_exam/exercitiu2.py
@@ -0,0 +1,21 @@
+def element_majoritar(x: list) -> list:
+    elemente = []
+    aparitie_maxima = 0
+    for value in x:
+        if x.count(value) > aparitie_maxima:
+            aparitie_maxima = x.count(value)
+    for value in x:
+        if x.count(value) == aparitie_maxima and str(value) not in elemente:
+            elemente.append(str(value))
+    if elemente:
+        return ','.join(elemente)
+    else:
+        return "Elementele majoritare nu au fost gasite."
+
+
+lista = input("Introduceti o lista de numere: ")
+lista = lista.split()
+while any(not x.isdigit() for x in lista) or len(lista) == 0:
+    lista = input("Introduceti o lista de numere: ")
+    lista = lista.split()
+print(element_majoritar(lista))
